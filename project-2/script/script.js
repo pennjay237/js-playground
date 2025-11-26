2c46b5463372805bad17b7fedc6578c4e0920d2f
pennjay237 <pennjude31@gmail.com>
2025-11-26 12:29:25 +0100

    applying the fetch to the news api

--- project-2/script/script.js
+++ project-2/script/script.js
@@ -1,18 +1,10 @@
-const url = 'http://worldtimeapi.org/api/timezone/America/New_York/';
+const api_token = "IJbXDHzuh5bsMKYthQZL2LTvAuPxifeXs1R0tUgp";
+const url = `https://api.thenewsapi.com/v1/news/top?api_token=${api_token}`;
 
 async function getData() {
     const response = await fetch(url);
-    const data = await response.json()
+    const data = await response.json();
     console.log(data);
-
 }
 
 getData();
-
-
-
-
-
-
-
-
