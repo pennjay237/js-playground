b284bf9cb53f25e34cc6b9fd98403470a4b03f25
pennjay237 <pennjude31@gmail.com>
2025-11-27 14:12:24 +0100

    project 3 on DOM manipulation

--- project-1/script/script.js
+++ project-1/script/script.js
@@ -12,23 +12,18 @@ let buttonEl = document.querySelector("button");
 let resultEl = document.querySelector("#result");
 
 buttonEl.addEventListener("click", () => {
-    let id = Number(inputEl.value);
+    let id = Number(inputEl.value); 
+
     let user = jsonData.find(item => item.id === id);
 
     resultEl.replaceChildren();
 
     if (user) {
         let nameEl = document.createElement("h2");
-        let ageEl = document.createElement("p");
-        let talentEl = document.createElement("p");
 
         nameEl.textContent = user.name;
-        ageEl.textContent = "Age: " + user.age;
-        talentEl.textContent = "Talent: " + user.talent;
 
         resultEl.appendChild(nameEl);
-        resultEl.appendChild(ageEl);
-        resultEl.appendChild(talentEl);
 
     } else {
         let notFoundEl = document.createElement("h3");
---
+++ project-3/script/script.js
@@ -0,0 +1,11 @@
+document.getElementById("changeTextBtn").addEventListener("click", () => {
+});
+
+document.getElementById("changeColorBtn").addEventListener("click", () => {
+});
+
+document.getElementById("addItemBtn").addEventListener("click", () => {
+});
+
+document.getElementById("toggleBoxBtn").addEventListener("click", () => {
+});
